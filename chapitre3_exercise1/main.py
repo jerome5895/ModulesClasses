52c546b4fea2b06fb83c075435ccaafcf67f1147
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-23 22:19:16 -0400

    Added chapitre1_exercise1

---
+++ chapitre3_exercise1/main.py
@@ -0,0 +1,25 @@
+"""Point d'entréé."""
+
+
+from contact.user import User
+from contact.owlcontact import OwlContactSystem
+from contact.textcontact import TextContactSystem
+from contact.helpers import send_mass_messages
+
+
+"""Our main program."""
+def main():
+    alice = User("Alice", TextContactSystem("0102030405"))
+    bob = User("Bob", OwlContactSystem("4 Privet Drive"))
+
+    user_list = [alice, bob]
+    send_mass_messages("Hello {name}, Comment vas-tu?", user_list)
+    send_mass_messages(
+        "Salut {name}. Tes informations de contact sont-elles corrects?"
+        " Nous avons celles-ci: {contact_info}.",
+        user_list,
+    )
+
+
+if __name__ == "__main__":
+    main()
