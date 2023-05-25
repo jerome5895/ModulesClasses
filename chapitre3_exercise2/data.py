721c6ca8bcbdc806077e3dd835b8b5f66675a203
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-05-24 20:21:59 -0400

    Renamed data.py

---
+++ chapitre3_exercise2/data.py
@@ -0,0 +1,30 @@
+films = [
+    ("Blade Runner (1982)", "vhf"),
+    ("Alien : Le 8ème Passager (1979)", "vhf"),
+    ("2001 : L'Odyssée de l'espace (1968)", "vhF"),
+    ("Matrix (1999)", "DVD"),
+    ("Interstellar (2014)", "dvD"),
+    ("L'Empire contre-attaque (1980)", "vhf"),
+    ("Retour vers le futur (1985)", "vhf"),
+    ("La Guerre des Etoiles (1977)", "vhf"),
+    ("L'Armée des 12 singes (1995)", "dVd"),
+    ("Terminator 2 : Le Jugement dernier (1991)", "DVD")
+]
+
+
+friends = [
+    ("Paul", "Blade Runner"),
+    ("Lucie",),
+    ("Zoé", "Terminator 2 : Le Jugement dernier"),
+]
+
+
+
+class Nettoyage:
+    def __init__(self, name, date, type):
+        self.name = name
+        self.date = date
+        self.type = type
+
+    def 
+
